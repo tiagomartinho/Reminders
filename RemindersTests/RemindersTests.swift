0cd2caf666a55888f26bb4434f14b4e2b18968f5
Tiago Martinho <tiago@elit.software>
2017-12-09 17:03:33 +0100

    Initial Commit

---
+++ RemindersTests/RemindersTests.swift
@@ -0,0 +1,36 @@
+//
+//  RemindersTests.swift
+//  RemindersTests
+//
+//  Created by Tiago Martinho on 9/12/17.
+//  Copyright © 2017 tm. All rights reserved.
+//
+
+import XCTest
+@testable import Reminders
+
+class RemindersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
