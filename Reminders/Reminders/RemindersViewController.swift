0cd1fa60f1810c3bc265d799d9f60e4f253a915c
Tiago Martinho <tiago@elit.software>
2017-12-10 21:31:25 +0100

    Display reminders with table view

--- Reminders/Reminders/RemindersViewController.swift
+++ Reminders/Reminders/RemindersViewController.swift
@@ -6,6 +6,8 @@ class RemindersViewController: UIViewController {
     private var router: Router!
     private var presenter: RemindersPresenter!
 
+    private var reminders: [Reminder]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
@@ -44,5 +46,31 @@ extension RemindersViewController: RemindersView {
     }
 
     func showReminders(reminders: [Reminder]) {
+        self.reminders = reminders
+        let tableView = UITableView()
+        tableView.accessibilityLabel = "Reminders"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        tableView.dataSource = self
+        view = tableView
+    }
+}
+
+extension RemindersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reminders.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewCell(tableView: tableView, reuseIdentifier: "UITableViewCell")
+        cell.textLabel?.text = reminders[indexPath.row].title
+        return cell
+    }
+
+    private func tableViewCell(tableView: UITableView, reuseIdentifier: String) -> UITableViewCell {
+        if let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) {
+            return dequeuedCell
+        } else {
+            return UITableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
+        }
     }
 }
--- RemindersUITests/RemindersEndToEndTest.swift
+++ RemindersUITests/RemindersEndToEndTest.swift
@@ -1,15 +1,38 @@
 import XCTest
 
 class RemindersEndToEndTest: XCTestCase {
-        
+
+    let app = XCUIApplication()
+
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
-        XCUIApplication().launch()
+        app.launch()
     }
     
     func testShowNoReminders() {
-        let noRemindersLabel = XCUIApplication().staticTexts["You have no reminders"]
+        assertNoReminders()
+        addReminder()
+        assertReminderIsPresent()
+    }
+
+    private func assertNoReminders() {
+        let noRemindersLabel = app.staticTexts["You have no reminders"]
         XCTAssert(noRemindersLabel.exists)
+    }
+
+    private func addReminder() {
+        app.navigationBars["Reminders"].buttons["Add"].tap()
+        let titleTextField = app.textFields["Title"]
+        titleTextField.tap()
+        titleTextField.typeText("Read Clean Architecture")
+        app.navigationBars["Add Reminder"].buttons["Save"].tap()
+    }
+
+    private func assertReminderIsPresent() {
+        let table = app.tables["Reminders"]
+        XCTAssert(table.exists)
+        XCTAssertEqual(1, table.cells.count)
+        XCTAssert(table.cells.staticTexts["Read Clean Architecture"].exists)
     }
 }
