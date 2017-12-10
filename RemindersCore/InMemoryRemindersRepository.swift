457f3f9febd3ae2ed5375e2470cebcea6b6ea165
Tiago Martinho <tiago@elit.software>
2017-12-10 21:20:04 +0100

    Implement InMemoryRemindersRepository

--- RemindersCore/InMemoryRemindersRepository.swift
+++ RemindersCore/InMemoryRemindersRepository.swift
@@ -1,12 +1,15 @@
 public class InMemoryRemindersRepository: RemindersRepository {
 
+    static var reminders = [Reminder]()
+
     public init() {
     }
     
     public func save(_ reminder: Reminder) {
+        InMemoryRemindersRepository.reminders.append(reminder)
     }
 
     public func load() -> [Reminder] {
-        return []
+        return InMemoryRemindersRepository.reminders
     }
 }
