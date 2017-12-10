5694bdc1323d2c9a67ec85a400e59c2ece9a65c7
Tiago Martinho <tiago@elit.software>
2017-12-10 22:37:50 +0100

    Extract ControllerFactory

--- Reminders/AppRouter.swift
+++ Reminders/AppRouter.swift
@@ -17,45 +17,17 @@ class AppRouter: Router {
     func route(to view: View) {
         switch view {
         case .addReminder:
-            controller?.present(viewController(from: view), animated: true, completion: nil)
+            let controllerToPresent = ControllerFactory.build(from: view, router: self)
+            controller?.present(controllerToPresent,
+                                animated: true, completion: nil)
+            controller = controllerToPresent
         case .reminders:
             if let _ = window?.rootViewController {
                 controller?.dismiss(animated: true, completion: nil)
-                controller = window?.rootViewController
             } else {
-                window?.rootViewController = viewController(from: view)
+                window?.rootViewController = ControllerFactory.build(from: view, router: self)
             }
+            controller = window?.rootViewController
         }
-    }
-
-    private func viewController(from view: View) -> UIViewController {
-        let rootViewController: UIViewController
-        switch view {
-        case .addReminder:
-            rootViewController = buildAddReminderViewController()
-        case .reminders:
-            rootViewController = buildRemindersViewController()
-        }
-        controller = UINavigationController(rootViewController: rootViewController)
-        return controller!
-    }
-
-    private func buildAddReminderViewController() -> AddReminderViewController {
-        let controller = AddReminderViewController()
-        let repository = InMemoryRemindersRepository()
-        let presenter = AddReminderPresenter(router: self,
-                                             repository: repository)
-        controller.presenter = presenter
-        return controller
-    }
-
-    private func buildRemindersViewController() -> RemindersViewController {
-        let controller = RemindersViewController()
-        let repository = InMemoryRemindersRepository()
-        let presenter = RemindersPresenter(view: controller,
-                                           router: self,
-                                           repository: repository)
-        controller.presenter = presenter
-        return controller
     }
 }
---
+++ Reminders/ControllerFactory.swift
@@ -0,0 +1,27 @@
+import UIKit
+import RemindersCore
+
+class ControllerFactory {
+
+    static let repository = InMemoryRemindersRepository()
+
+    static func build(from view: View, router: Router) -> UIViewController {
+        let rootViewController: UIViewController
+        switch view {
+        case .addReminder:
+            let controller = AddReminderViewController()
+            let presenter = AddReminderPresenter(router: router,
+                                                 repository: repository)
+            controller.presenter = presenter
+            rootViewController = controller
+        case .reminders:
+            let controller = RemindersViewController()
+            let presenter = RemindersPresenter(view: controller,
+                                               router: router,
+                                               repository: repository)
+            controller.presenter = presenter
+            rootViewController = controller
+        }
+        return UINavigationController(rootViewController: rootViewController)
+    }
+}
