525cdc342146ff7f5ec36a2933d00163692c6ed7
r é m i  <remi.robert@epitech.eu>
2017-12-11 21:50:05 +0800

    add dependency injection for the controller factory router

--- Reminders/AppRouter.swift
+++ Reminders/AppRouter.swift
@@ -2,18 +2,20 @@ import RemindersCore
 import UIKit
 
 class AppRouter: Router {
-
     private var controller: UIViewController?
     private var window: UIWindow?
+    private let controllerFactory: ControllerFactory
 
-    init(window: UIWindow) {
+    init(window: UIWindow,
+         controllerFactory: ControllerFactory = ControllerFactory()) {
         self.window = window
+        self.controllerFactory = controllerFactory
     }
 
     func route(to view: View) {
         switch view {
         case .addReminder:
-            let controllerToPresent = ControllerFactory.build(from: view, router: self)
+            let controllerToPresent = controllerFactory.build(from: view, router: self)
             controller?.present(controllerToPresent,
                                 animated: true, completion: nil)
             controller = controllerToPresent
@@ -21,7 +23,7 @@ class AppRouter: Router {
             if window?.rootViewController != nil {
                 controller?.dismiss(animated: true, completion: nil)
             } else {
-                window?.rootViewController = ControllerFactory.build(from: view, router: self)
+                window?.rootViewController = controllerFactory.build(from: view, router: self)
             }
             controller = window?.rootViewController
         }
