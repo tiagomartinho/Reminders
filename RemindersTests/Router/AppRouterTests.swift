90172c1f9524145c6d6bd291363c2e8992a90117
Tiago Martinho <tiago@elit.software>
2017-12-11 20:04:09 +0100

    Extract SpyViewController to separate file

--- RemindersTests/Router/AppRouterTests.swift
+++ RemindersTests/Router/AppRouterTests.swift
@@ -30,18 +30,3 @@ class AppRouterTests: XCTestCase {
         XCTAssertEqual(fakeWindow.rootViewController!, reminderController)
     }
 }
-
-class SpyViewController: UIViewController {
-    private(set) var dismissed = false
-    private(set) var presenting = false
-
-    override func present(_ viewControllerToPresent: UIViewController,
-                          animated flag: Bool, completion: (() -> Void)? = nil) {
-        presenting = true
-        super.present(viewControllerToPresent, animated: flag, completion: completion)
-    }
-
-    override func dismiss(animated _: Bool, completion _: (() -> Void)? = nil) {
-        dismissed = true
-    }
-}
