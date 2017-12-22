0f7c69eba2a12583fa67b5f7ad6a765401bb503d
Tiago Martinho <tiago@elit.software>
2017-12-22 14:14:03 +0000

    Format code

--- Reminders/Router/AppRouter.swift
+++ Reminders/Router/AppRouter.swift
@@ -15,7 +15,8 @@ class AppRouter: Router {
     func route(to route: Route) {
         let controllerToPresent = controllerFactory.build(from: route,
                                                           with: self)
-        let router = ControllerRouterFactory(controller: controller, window: window).build(from: route)
+        let router = ControllerRouterFactory(controller: controller,
+                                             window: window).build(from: route)
         controller = router.route(to: controllerToPresent)
     }
 }
