ac464e3e163cf2559f9c7494e1aa35bc6ded3a99
Tiago Martinho <t.martinho@icloud.com>
2018-10-03 09:39:12 +0200

    Migrate to Swift 4.2

--- Reminders/AppDelegate.swift
+++ Reminders/AppDelegate.swift
@@ -6,7 +6,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication,
-                     didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
         window?.makeKeyAndVisible()
         let router = AppRouter(window: window!,
